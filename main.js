9627a5d1b49a57521bf97191939d186b6936e0d1
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-07-16 17:39:51 +0530

    Use html / css / Js from ProtoGraph CMS index page and get an data.html up from JSON #2

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './src/js/List.jsx';
+
+ReactDOM.render(<List dataURL='src/data/data.json'/>, document.getElementById('card-list-div'));
+
---
+++ src/js/List.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+export default class ListCards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataJSON: undefined
+    }
+  }
+
+  componentDidMount() {
+    axios.get(this.props.dataURL)
+      .then(cards_data => {
+        this.setState({
+          dataJSON: cards_data.data
+        });
+    });
+  }
+
+  render() {
+    if (this.state.dataJSON === undefined) {
+      return(<div>Loading</div>)
+    } else {
+      let cards = this.state.dataJSON.map((card, i) => {
+        return(
+          <div key={i} className="protograph-card">
+            {card.image ? <img className="card-image" src={card.image} width='100%'/> : <div className="empty-card" width='100%'></div>}
+            <div className="protograph-gradient">
+              <div className="data-card-content">
+                <div className="data-card-title">{card.title}</div>
+                <div className="data-card-date">{card.date}</div>
+              </div>
+            </div>
+          </div>
+        )
+      })
+      return(
+        <div className="protograph-container">
+          <div className="protograph-card-area">{cards}</div>
+        </div>
+      )
+    }
+  }
+}
