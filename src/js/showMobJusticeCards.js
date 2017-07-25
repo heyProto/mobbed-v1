d1daa0bd470615548720a776b93ca3d3163cb799
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-07-25 14:54:07 +0530

    updated cloudfront data url

--- src/js/showMobJusticeCards.js
+++ src/js/showMobJusticeCards.js
@@ -13,7 +13,7 @@ function getJSON(url, callback) {
   xhr.send();
 };
 
-getJSON('https://dkqrqc7q64awx.cloudfront.net/toMobJustice/index.json', function (err, data){
+getJSON('https://dwln9tzsi7g07.cloudfront.net/toMobJustice/index.json', function (err, data){
   if (err != null) {
     alert('Something went wrong: ' + err);
   } else {
