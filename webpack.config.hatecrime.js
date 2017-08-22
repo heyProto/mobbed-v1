dd7f29f0378365b4feafb71aa446236bafe61350
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2017-08-22 13:43:22 +0530

    added minification

--- webpack.config.hatecrime.js
+++ webpack.config.hatecrime.js
@@ -16,6 +16,9 @@ module.exports = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
   ],
   node: {
     net: 'empty',
